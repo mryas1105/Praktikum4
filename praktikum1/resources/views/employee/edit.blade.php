92f14778633d0ade59e491b938affa3e4eef9e1d
MR <57211612+mryas1105@users.noreply.github.com>
2024-04-01 15:01:32 +0700

    Add files via upload

---
+++ praktikum1/resources/views/employee/edit.blade.php
@@ -0,0 +1,69 @@
+{{-- <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Employee - {{ $employee->firstname }} {{ $employee->lastname }}</title>
+    @vite('resources/sass/app.scss')
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+        <div class="container">
+            <a href="{{ route('home') }}" class="navbar-brand mb-0 h1"><i class="bi-hexagon-fill me-2"></i> Data Master</a>
+
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <hr class="d-lg-none text-white-50">
+
+                <ul class="navbar-nav flex-row flex-wrap">
+                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('home') }}" class="nav-link">Home</a></li>
+                    <li class="nav-item col-2 col-md-auto"><a href="{{ route('employees.index') }}" class="nav-link">Employee List</a></li>
+                </ul>
+
+                <hr class="d-lg-none text-white-50">
+
+                <a href="{{ route('profile') }}" class="btn btn-outline-light my-2 ms-md-auto"><i class="bi-person-circle me-1"></i> My Profile</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="container mt-4">
+        <h4>Edit Employee - {{ $employee->firstname }} {{ $employee->lastname }}</h4>
+        <hr>
+        <form action="{{ route('employees.', ['employee' => $employee->employee_id]) }}" method="POST">
+            @csrf
+            @method('put')
+            <div class="mb-3">
+                <label for="firstname" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="firstname" name="firstname" value="{{ $employee->firstname }}">
+            </div>
+            <div class="mb-3">
+                <label for="lastname" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lastname" name="lastname" value="{{ $employee->lastname }}">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" name="email" value="{{ $employee->email }}">
+            </div>
+            <div class="mb-3">
+                <label for="age" class="form-label">Age</label>
+                <input type="number" class="form-control" id="age" name="age" value="{{ $employee->age }}">
+            </div>
+            <div class="mb-3">
+                <label for="position" class="form-label">Position</label>
+                <input type="text" class="form-control" id="position" name="position" value="{{ $employee->position_name }}">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+
+    @vite('resources/js/app.js')
+</body>
+
+</html> --}}
---
+++ praktikum1/storage/framework/views/2061e863e2b0ade1d71a8d74dd43d29f.php
@@ -0,0 +1,2 @@
+
+<?php /**PATH C:\xampp\htdocs\praktikum1\resources\views/employee/edit.blade.php ENDPATH**/ ?>
